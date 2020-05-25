3d2a7e0fa97084074c72c7293a5022a5c4ebf7b8
chentong <531054328@qq.com>
2020-05-25 17:42:38 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './common/css/reset.css'
+import './common/css/index.css'
+import axios from 'axios'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
