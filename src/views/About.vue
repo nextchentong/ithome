b306cf079603b8b9e82c74e34dcc6840b9e4dc0b
chentong <531054328@qq.com>
2020-05-28 16:24:45 +0800

    ithome - content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,53 @@
 <template>
   <div class="content">
-    <iframe class="iframe"
-            :src="href"></iframe>
+    <h1 class="title">{{title}}</h1>
+    <div v-html="content">
+
+    </div>
+
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      href: ''
+      href: '',
+      content: '',
+      title: ''
     }
   },
   created() {
     this.href = this.$route.query.href
+    this.title = this.$route.query.title
+    this.$axios
+      .post('https://undefined.net.cn/api/ithome-content', {
+        href: this.href
+      })
+      .then(response => {
+        console.log(response)
+        this.content = response.data
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
     console.log(this.href)
   }
 }
 </script>
 <style lang="stylus" scoped>
 .content
-  height 100vh
-  .iframe
+  color #fff
+  padding 5%
+  box-sizing border-box
+  width 100vw
+  overflow hidden
+  .title
+    font-size 30px
+    margin-bottom 10%
+  >>> p
+    line-height 1.5
+    margin 2% 0
+  >>> img
     width 100%
-    height 100%
+    margin 5% 0
 </style>
