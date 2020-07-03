8b27284b319a87fda1b3fdce3188aa07df5deb98
chentong <531054328@qq.com>
2020-07-03 15:38:18 +0800

    修复keep=alive 缓存所有组件的bug exclude的名字是组件的名字  不是路由的name

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <router-view></router-view>
+    <keep-alive exclude ="About">
+      <router-view ></router-view>
     </keep-alive>
 
   </div>
--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,7 @@
 </template>
 <script>
 export default {
+  name: 'About',
   data() {
     return {
       href: '',
